a2ea4d6978d2c198e394d9f8a2d688bd40fae38e
Udaya Singh Bhadauriya <udaya@jaysingh.com>
2026-03-31 23:52:08 -0500

    Update base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,9 @@
-import { defineConfig } from 'vite'
-import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
-    react(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      // Alias @ to the src directory
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
-  assetsInclude: ['**/*.svg', '**/*.csv'],
-})
+  base: '/Ccshubfigma/',
+  plugins: [vue()],
+});
